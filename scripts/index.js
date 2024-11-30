ff735b781fb479cd428c2511e2f99e06c774bc83
KEVIN ALCANTARA <kevinalcantara992@gmail.com>
2024-11-30 01:20:22 -0500

    stage 1

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+const initialCards = [
+  {
+    name: "Georgie",
+    link: "https://unsplash.com/photos/a-close-up-of-a-dog-with-a-mountain-in-the-background-ALG0gP4GDbw",
+  },
+  {
+    name: "Living Space",
+    link: "https://unsplash.com/photos/a-living-room-filled-with-furniture-and-a-staircase-PuTKvRO7Fjs",
+  },
+  {
+    name: "Wandering",
+    link: "https://unsplash.com/photos/a-man-is-walking-down-a-cobblestone-street-dGJEFS8wTFw",
+  },
+  {
+    name: "Transport",
+    link: "https://unsplash.com/photos/a-black-sports-car-driving-down-a-road-Ls0ZPURpoDE",
+  },
+  {
+    name: "Rechage",
+    link: "https://unsplash.com/photos/a-table-topped-with-a-notebook-and-a-cup-of-tea-b1nKa1IAzI0",
+  },
+  {
+    name: "Ending a End",
+    link: "https://unsplash.com/photos/a-group-of-people-walking-across-a-bridge-fF1DWEA1qB8",
+  },
+];
+
+console.log(initialCards);
