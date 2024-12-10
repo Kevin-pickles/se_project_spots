9a6a9bdd8794a147af63bc3d14ffde41b39c965a
KEVIN ALCANTARA <kevinalcantara992@gmail.com>
2024-12-09 20:50:10 -0500

    first submission part 4

--- scripts/index.js
+++ scripts/index.js
@@ -1,42 +1,80 @@
 const initialCards = [
   {
     name: "Georgie",
-    link: "https://unsplash.com/photos/a-close-up-of-a-dog-with-a-mountain-in-the-background-ALG0gP4GDbw",
+    link: "https://images.unsplash.com/photo-1732521028694-2c1908915495?q=80&w=1631&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "Living Space",
-    link: "https://unsplash.com/photos/a-living-room-filled-with-furniture-and-a-staircase-PuTKvRO7Fjs",
+    link: "https://images.unsplash.com/photo-1732640452152-8cca8e1d68ef?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "Wandering",
-    link: "https://unsplash.com/photos/a-man-is-walking-down-a-cobblestone-street-dGJEFS8wTFw",
+    link: "https://images.unsplash.com/photo-1731223833744-7904a1bbeb6e?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "Transport",
-    link: "https://unsplash.com/photos/a-black-sports-car-driving-down-a-road-Ls0ZPURpoDE",
+    link: "https://images.unsplash.com/photo-1732624697703-c5b0d3110cb4?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
-    name: "Rechage",
-    link: "https://unsplash.com/photos/a-table-topped-with-a-notebook-and-a-cup-of-tea-b1nKa1IAzI0",
+    name: "Recharge",
+    link: "https://plus.unsplash.com/premium_photo-1732018941995-64612d94991c?q=80&w=1602&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
-    name: "Ending a End",
-    link: "https://unsplash.com/photos/a-group-of-people-walking-across-a-bridge-fF1DWEA1qB8",
+    name: "Ending of a End",
+    link: "https://images.unsplash.com/photo-1732192548673-e08daf9595cc?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 ];
 
 const profileEditButton = document.querySelector(".profile__edit-btn");
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
 
 const editModal = document.querySelector("#edit-modal");
+const editFormElement = editModal.querySelector(".modal__form");
 const editModalCloseBtn = editModal.querySelector(".modal__close-btn");
+const editModalNameInput = editModal.querySelector("#profile-name-input");
+const editModalDescriptionInput = editModal.querySelector(
+  "#profile-description-input"
+);
+
+const cardTemplate = document.querySelector("#card-template");
+const cardsList = document.querySelector(".cards__list");
+
+function getCardElement(data) {
+  const cardElement = cardTemplate.content
+    .querySelector(".card")
+    .cloneNode(true);
+
+  const cardNameEl = cardElement.querySelector(".card__title");
+  cardNameEl.textContent = data.name;
+  const cardImageEl = cardElement.querySelector(".card__image");
+  cardImageEl.src = data.link;
+  cardImageEl.alt = data.name;
+
+  return cardElement;
+}
 
 function openModal() {
+  editModalNameInput.value = profileName.textContent;
+  editModalDescriptionInput.value = profileDescription.textContent;
   editModal.classList.add("modal_opened");
 }
 function closeModal() {
   editModal.classList.remove("modal_opened");
 }
 
-profileEditButton.addEventListener("click", openModal);
+function handleEditFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = editModalNameInput.value;
+  profileDescription.textContent = editModalDescriptionInput.value;
+  closeModal();
+}
 
+profileEditButton.addEventListener("click", openModal);
 editModalCloseBtn.addEventListener("click", closeModal);
+editFormElement.addEventListener("submit", handleEditFormSubmit);
+
+for (let i = 0; i < initialCards.length; i++) {
+  const cardElement = getCardElement(initialCards[i]);
+  cardsList.prepend(cardElement);
+}
